3093cc3878994bd3a50126499c2aaa044c1b81cc
Gabriel Carneiro <gabrielcarneiro.melo@yahoo.com.br>
2019-12-05 10:43:09 -0300

    AvaliacaoAutomacao/

--- AvaliacaoAutomacao/src/main/java/br/com/inmetrics/AvaliacaoAutomacao/webservice/GetTest.java
+++ AvaliacaoAutomacao/src/main/java/br/com/inmetrics/AvaliacaoAutomacao/webservice/GetTest.java
@@ -5,19 +5,25 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.junit.Test;
+import org.springframework.data.web.JsonPath;
 
 public class GetTest {
 	
 	@Test
 	public void requisicaoGet() throws ClientProtocolException, IOException {
 	
+	
+		
 		// Given
 	    HttpUriRequest request = new HttpGet("http://swapi.co/api/films/");
 	 
@@ -27,9 +33,14 @@ public class GetTest {
 	    
 	    
 	    
-	    // Then
+	    HttpEntity entity = httpResponse.getEntity();
+	    String responseString = EntityUtils.toString(entity, "UTF-8");
+	    System.out.println(responseString);
+	       
+        
+        // Then
 	    assertThat(
-	      httpResponse.getStatusLine().getStatusCode(),equalTo(HttpStatus.SC_OK));
+	    httpResponse.getStatusLine().getStatusCode(),equalTo(HttpStatus.SC_OK));
 		
 	      
 	}
